c8afda89613964352d531f9648ace23c410bf92e
Reyabdul <reyabdul@gmail.com>
2023-06-14 15:17:35 -0400

    Add project files.

---
+++ FilmPhotoJournalApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FilmPhotoJournalApp.Startup))]
+namespace FilmPhotoJournalApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
