7aa39726f0b6ab571d950398e75948ea35b9cbc1
Reyabdul <reyabdul@gmail.com>
2023-06-28 15:27:56 -0400

    Add and test all crud functions to see if they connect to all api end points.

---
+++ FilmPhotoJournalApp/Controllers/PhotoDataController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FilmPhotoJournalApp.Models;
+//use to help with debugging **use the output terminal to see repponse fromd debug
+using System.Diagnostics;
+
+namespace FilmPhotoJournalApp.Controllers
+{
+    public class PhotoDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/PhotoData/ListPhotos
+        [HttpGet]
+        public IEnumerable<PhotoDto> ListPhotos()
+        {
+            List<Photo> Photos = db.Photos.ToList();
+            List<PhotoDto> PhotoDtos = new List<PhotoDto>();
+
+            Photos.ForEach(p => PhotoDtos.Add(new PhotoDto()
+            {
+                PhotoID = p.PhotoID,
+                Iso = p.Iso,
+                CollectionTitle = p.Collection.CollectionTitle
+            }));
+
+            return PhotoDtos;
+        }
+
+        // GET: api/FindPhoto/5
+        [ResponseType(typeof(Photo))]
+        [HttpGet]
+        public IHttpActionResult FindPhoto(int id)
+        {
+            Photo photo = db.Photos.Find(id);
+            PhotoDto PhotoDto = new PhotoDto()
+            {
+                PhotoID = photo.PhotoID,
+                Iso = photo.Iso,
+                CollectionTitle = photo.Collection.CollectionTitle
+            };
+            if (photo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(PhotoDto);
+        }
+
+        // POST: api/PhotoData/UpdatePhoto/5
+        /*
+         HOW TO TEST UPDATING PHOTOS:   
+         -update the json file with the data
+         -in 'C:\Users\reyabdul\source\repos\FilmPhotoJournalApp\FilmPhotoJournalApp\jsondata', type 'curl -d @photo.json -H "Content-type:application/json" "https://localhost:44350/api/photodata/updatephoto/6"'
+         -check by typing 'curl https://localhost:44350/api/photodata/findphotos/5'
+
+         */
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        public IHttpActionResult UpdatePhoto(int id, Photo photo)
+        {
+            Debug.WriteLine("I have reached update photo method.");
+            if (!ModelState.IsValid)
+            {
+                Debug.WriteLine("Model State is invalid");
+                return BadRequest(ModelState);
+            }
+
+            if (id != photo.PhotoID)
+            {
+                Debug.WriteLine("ID mismatch");
+                Debug.WriteLine("Get parameter" + id);
+                Debug.WriteLine("POST parameter" + photo.PhotoID);
+                Debug.WriteLine("POST parameter" + photo.Iso);
+                return BadRequest();
+            }
+
+            db.Entry(photo).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PhotoExists(id))
+                {
+                    Debug.WriteLine("Photo not found");
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            Debug.WriteLine("None of the conditions triggered");
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+
+
+        // POST: api/PhotoData/AddPhoto
+        /*
+         HOW TO TEST ADDING PHOTO:
+        -create folder 'json data' with json file (e.g.:photo.json)
+        -send post request, open photo.json in open folder in file explorer, cop addressas text, open command line and cd into address (e.g.:C:\Users\reyabdul\source\repos\FilmPhotoJournalApp\FilmPhotoJournalApp\jsondata)
+        -command line:
+            -type 'dir' and check for photo.json
+            -add animal:
+                -curl -d @photo.json -H "Content-type:application/json" https://localhost:44350/api/photodata/addphoto
+                -check if add by typing 'curl https://localhost:44350/api/photodata/listphotos'       
+         */
+        [ResponseType(typeof(Photo))]
+        [HttpPost]
+        public IHttpActionResult AddPhoto(Photo photo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Photos.Add(photo);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = photo.PhotoID }, photo);
+        }
+
+        // POST: api/PhotoData/DeletePhoto/5
+            //Can test delete is working by typing in the command line 'curl -d "" https://localhost:44350/api/photodata/deletephoto/5
+        [ResponseType(typeof(Photo))]
+        [HttpPost]
+        public IHttpActionResult DeletePhoto(int id)
+        {
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return NotFound();
+            }
+
+            db.Photos.Remove(photo);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PhotoExists(int id)
+        {
+            return db.Photos.Count(e => e.PhotoID == id) > 0;
+        }
+    }
+}
--- FilmPhotoJournalApp/Models/Photo.cs
+++ FilmPhotoJournalApp/Models/Photo.cs
@@ -14,7 +14,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 -after creating the DbSets, migration, and pushing into the database, can update properties futher
 -can create other Models or foreign keys
 -when promted with red squiggly line, pressing 'alt+enter' wil display a recommended solution
-    
+-Error may occur
+    <Message>An error has occurred.</Message>
+    <ExceptionMessage>The 'ObjectContent`1' type failed to serialize the response body for content type 'application/xml; charset=utf-8'.</ExceptionMessage>    
+    -since photo model contain many information that also connects to foreign keys, we can create a simpler version of that class or 'data transfer object' which can be seen below in 'PhotoDto'
 */
 
 namespace FilmPhotoJournalApp.Models
@@ -29,8 +32,6 @@ namespace FilmPhotoJournalApp.Models
         //Todo: av, ss, notes, data, datetaken,image
 
 
-
-
         //--------Foreign Keys---------------  
 
         //A photo belongs to one collection
@@ -41,5 +42,12 @@ namespace FilmPhotoJournalApp.Models
         //if error occurs when updating database, it might be the cause of having existing values in the table, clear them and it should resolve the issue
         public virtual Collection Collection { get; set; } //error may occur when creating migration that connects photo and collections (e.g.:photo-collection). This is sometimes do to Entity requiring to be more explicit when describing a naviagation proprty(should be okay in other databases)
 
+    }
+
+    public class PhotoDto
+    {
+        public int PhotoID { get; set; }
+        public int Iso { get; set; }
+        public string CollectionTitle { get; set; }
     }
 }
